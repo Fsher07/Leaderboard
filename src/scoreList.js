c311e66b8782e55a3a108e7cae5b64420ada91f3
Fsher07 <furkansalimm@gmail.com>
2022-04-06 14:36:09 +0300

    Uptade readme file

--- src/scoreList.js
+++ src/scoreList.js
@@ -24,11 +24,7 @@ export default class scoreBoard {
   static displayScores = (input) => {
     const scoreList = document.querySelector('.scoreList');
     const scoreListItem = document.createElement("li");
-    if (scoreBoard.scores.indexOf(input) % 2 === 0) {
-      scoreListItem.style.backgroundColor = '#D3D3D3';
-    } else {
-      scoreListItem.style.backgroundColor = '#fff';
-    }
+    scoreListItem.className = "scoreListItem";
     scoreListItem.innerHTML = `${input.person} : ${input.score}`;
     scoreList.appendChild(scoreListItem);
   }
