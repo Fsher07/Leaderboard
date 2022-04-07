fd38d3a1c889d8a617188e3af7eb2f90aa8e21f3
Fsher07 <furkansalimm@gmail.com>
2022-04-07 16:20:20 +0300

    Update api-game.js

--- src/api-game.js
+++ src/api-game.js
@@ -8,10 +8,23 @@ async function postGame() {
     },
     body: JSON.stringify({name: 'My cool new game'}),
   });
-  console.log(result.json());
+  return result.json();
 }
 
 createGame().then((response) => {
   [gameID] = response.result.match(/(?<=ID: )[^ ]+/);
 });
+
+async function addScoreItem (input) {
+  const scoreItem = await fetch(`https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${gameID}/scores`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(input),
+  });
+  return scoreItem.json();
+}
+
+
 
